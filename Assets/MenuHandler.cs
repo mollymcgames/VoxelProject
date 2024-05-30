93b371bb971da4f61bd3307a747e783b6749511f
Molly <mollymcconaghy@gmail.com>
2024-05-30 21:14:59 +0100

    scene load next

---
+++ Assets/MenuHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("World");
+    }
+}
