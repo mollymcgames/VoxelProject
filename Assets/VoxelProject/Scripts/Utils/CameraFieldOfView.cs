788aa65d02b3b9ae63963ea4194ba77ae5430e41
molly.mcconaghy@gmail.com <molly.mcconaghy@gmail.com>
2024-06-30 22:00:57 +0100

    Made fov slider also work with holding-down mousewheel

--- Assets/VoxelProject/Scripts/Utils/CameraFieldOfView.cs
+++ Assets/VoxelProject/Scripts/Utils/CameraFieldOfView.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.Rendering.PostProcessing;
 
 public class CameraFieldOfView : MonoBehaviour
 {
     //This is the field of view that the Camera has
     float m_FieldOfView;
+    public float sensitivity = 0.1f; // Sensitivity of mouse movement
+    private bool isDragging = false;
+    private float lastMouseY;
+
+    //Set up the maximum and minimum values the Slider can return (you can change these)
+    float max = 150.0f;
+    float min = 20.0f;
+
+
     //public Camera mainCamera;
-/*    public PostProcessVolume postProcessVolume;
-    private UnityEngine.Rendering.PostProcessing.DepthOfField depthOfField;*/
+    /*    public PostProcessVolume postProcessVolume;
+        private UnityEngine.Rendering.PostProcessing.DepthOfField depthOfField;*/
 
     void Start()
     {
@@ -17,12 +24,33 @@ public class CameraFieldOfView : MonoBehaviour
 /*
         // Get the Depth of Field effect from the Post Process Volume
         postProcessVolume.profile.TryGetSettings(out depthOfField);*/
-
-
     }
 
     void Update()
     {
+        // Check if the mouse wheel button is pressed
+        if (Input.GetMouseButtonDown(2))
+        {
+            isDragging = true;
+            lastMouseY = Input.mousePosition.y;
+        }
+
+        // Check if the mouse wheel button is released
+        if (Input.GetMouseButtonUp(2))
+        {
+            isDragging = false;
+        }
+
+        // Update the slider value based on mouse movement if dragging
+        if (isDragging)
+        {
+            float mouseY = Input.mousePosition.y;
+            float deltaY = lastMouseY - mouseY;
+            m_FieldOfView += deltaY * sensitivity;
+            m_FieldOfView = Mathf.Clamp(m_FieldOfView, min, max);
+            lastMouseY = mouseY;
+        }
+
         //Update the camera's field of view to be the variable returning from the Slider
         Camera.main.fieldOfView = m_FieldOfView;
 
@@ -37,10 +65,6 @@ public class CameraFieldOfView : MonoBehaviour
 
     void OnGUI()
     {
-        //Set up the maximum and minimum values the Slider can return (you can change these)
-        float max, min;
-        max = 150.0f;
-        min = 20.0f;
         //This Slider changes the field of view of the Camera between the minimum and maximum values
         m_FieldOfView = GUI.HorizontalSlider(new Rect(20, 20, 100, 40), m_FieldOfView, min, max);
     }
